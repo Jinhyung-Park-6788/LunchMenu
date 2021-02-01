46fbc0ea301869e7dadbe7a85a1076adb3c63cc8
Jinhyung <supermanx3@naver.com>
2021-02-01 23:10:36 +0900

    210201-v1.0.1 deploy.sh update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -31,7 +31,12 @@ chmod +x $JAR_NAME
 
 echo "> Execution JAR"
 
+#nohup java -jar \
+# -Dspring.config.location=classpath:/application.properties,classpath=:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+# -Dspring.profiles.active=real \
+# $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
 nohup java -jar \
- -Dspring.config.location=classpath:/application.properties,classpath=:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
- -Dspring.profiles.active=real \
- $JAR_NAME > $REPOSITORY/nohub.out 2>&1 &
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
