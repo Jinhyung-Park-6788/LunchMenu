069f178ae6d874da44a4a31a89deee83117eb8cb
Jinhyung <supermanx3@naver.com>
2021-02-02 23:11:53 +0900

    210202-v1.0.1 deploy.sh update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,40 +1,79 @@
 #!/bin/bash
 
+#
+#REPOSITORY=/home/ec2-user/app/step2
+#PROJECT_NAME=freelec-springboot2-webservice
+#
+#echo "> Copy Build File"
+#cp $REPOSITORY/zip/*.jar $REPOSITORY/
+#
+#echo "> Check running process PID"
+#CURRENT_PID=$(pgrep -f1 freelec-springboot2-webservice | grep jar | awk '{print $1}')
+#
+#echo "Now running process PID: $CURRENT_PID"
+#
+#if [ -z "$CURRENT_PID" ]; then
+#  echo "> It doesn't exist running process."
+#else
+#  echo "> kill -15 $CURRENT_PID"
+#  kill  -15 $CURRENT_PID
+#  sleep 5
+#fi
+#
+#echo "> Deploy New application"
+#
+#JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+#
+#echo "> JAR name : $JAR_NAME"
+#
+#echo "> $JAR_NAME add execution permission"
+#
+#chmod +x $JAR_NAME
+#
+#echo "> Execution JAR"
+#
+##nohup java -jar \
+## -Dspring.config.location=classpath:/application.properties,classpath=:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+## -Dspring.profiles.active=real \
+## $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+#
+#nohup java -jar \
+#    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+#    -Dspring.profiles.active=real \
+#    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
 REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=freelec-springboot2-webservice
 
-echo "> Copy Build File"
+echo "> Build 파일 복사"
+
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> Check running process PID"
-CURRENT_PID=$(pgrep -f1 freelec-springboot2-webservice | grep jar | awk '{print $1}')
+echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-echo "Now running process PID: $CURRENT_PID"
+CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
+
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-  echo "> It doesn't exist running process."
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill  -15 $CURRENT_PID
-  sleep 5
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
-echo "> Deploy New application"
+echo "> 새 어플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR name : $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
-echo "> $JAR_NAME add execution permission"
+echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
-echo "> Execution JAR"
-
-#nohup java -jar \
-# -Dspring.config.location=classpath:/application.properties,classpath=:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-# -Dspring.profiles.active=real \
-# $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+echo "> $JAR_NAME 실행"
 
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
